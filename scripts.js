99ecb99b67086d31fea97bcb747e764db15d43c6
Oleg Danilyuk <danilyuk_oleg@vk.com>
2022-08-15 14:35:21 +0300

    выполнил

--- scripts.js
+++ scripts.js
@@ -1,18 +1,71 @@
-"use strict";
+const time = document.querySelector(".time");
+const timeSmall = document.querySelector(".time-small");
+const arr = [
+  "Январь",
+  "Февраль",
+  "Март",
+  "Апрель",
+  "Май",
+  "Июль",
+  "Август",
+  "Сентябрь",
+  "Октябрь",
+  "Ноябрь",
+  "Декабрь",
+];
 
-let arr = ["123", "20", "50", "49", "3000", "4000", "577889"];
+const arrDay = [
+  "Воскресенье",
+  "Понедельник",
+  "Вторник",
+  "Cреда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+];
 
-for (let elem of arr) {
-  if (elem[0] == 2 || elem[0] == 4) {
-    console.log(elem);
+const declOfNum = function (value, arr) {
+  value = Math.abs(value) % 100;
+  if (value >= 5 || value <= 20) {
+    return arr[1];
+  } else if (value === 1 || value === 21) {
+    return arr[0];
+  } else if ((value >= 2 && value <= 4) || value === 22 || value === 23) {
+    return arr[2];
   }
-}
-let n = 100;
+};
 
-nextPrime: for (let i = 1; i <= n; i++) {
-  for (let j = 2; j < i; j++) {
-    if (i % j == 0) continue nextPrime;
+const addZero = function (num) {
+  if (num >= 0 && num <= 9) {
+    return "0" + num;
+  } else {
+    return num;
   }
+};
 
-  console.log(`Делители этого числа: 1 и ${i}`);
-}
+const startTime = function () {
+  const data = new Date();
+
+  time.innerHTML = `<b>Сегодня ${arrDay[data.getDay()]}, ${data.getDate()} ${
+    arr[data.getMonth() - 1]
+  } ${data.getFullYear()} года, ${data.getHours()} ${declOfNum(
+    data.getHours(),
+    ["час", "часов", "часа"]
+  )} ${data.getMinutes()} ${declOfNum(data.getMinutes(), [
+    "минута",
+    "минут",
+    "минуты",
+  ])} ${data.getSeconds()} ${declOfNum(data.getSeconds(), [
+    "секунда",
+    "секунд",
+    "секунды",
+  ])}</b>`;
+
+  timeSmall.innerHTML = `<p>${addZero(data.getDate())}-${addZero(
+    data.getMonth() + 1
+  )}-${addZero(data.getFullYear())} - ${addZero(data.getHours())}:${addZero(
+    data.getMinutes()
+  )}:${addZero(data.getSeconds())}</p>`;
+};
+
+setInterval(startTime, 1000);
